5ad911949452ea9040d180ea147c11f5ba1bfdb2
oficialgustavo <37318115+oficialgustavo@users.noreply.github.com>
2018-03-15 14:15:21 -0300

    Update leitura_identificacao.py

--- leitura_identificacao.py
+++ leitura_identificacao.py
@@ -1,15 +1,3 @@
-"""
-    IMPORTANT: This program works only with two archives
-    Collect the name of two files from the user (.java archives)
-    Verify the existance of this files
-    Verify number of lines of each file
-    Verify if a word is in the file, count how many times it appears and identify where
-    Identify the program with Syntax Error (more send/recv)
-    ***Create a dictionary to save the lines and the commands
-    ***Create an archive (.txt) with a code to GraphViz
-    ***Define the conections between the two programs
-    ---tentar abrir o GraphViz--- (Não feito)
-"""
 import os #impotação do módulo OS para abrir arquivos
 
 subGraph=0
